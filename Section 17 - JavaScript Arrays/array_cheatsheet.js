36fd8d25ec99845e4904df5725ff53ce673f0a4c
Ethan Goddard <ethangoddard2009@gmail.com>
2021-01-13 13:55:17 -0500

    Section 17 completed

---
+++ Section 17 - JavaScript Arrays/array_cheatsheet.js
@@ -0,0 +1,25 @@
+// Making an array:
+const colors = ["red", "orange", "yellow"];
+
+// Arrays are indexed like strings:
+colors[0]; // "red"
+
+// They have a length:
+colors.length; //3
+
+// Important array methods:
+//push(value) - adds value to the END of an array
+//pop() - removes and returns last value in array
+
+//unshift(value) - adds value to START of an array
+//shift() - removes and returns first element in an array
+
+// Common Array Methods
+// concat - merge arrays
+// includes - look for a value
+// indexOf - just like string.indexOf
+// join - creates a string from an array
+// reverse - reverses an array
+// slice - copies a portion on an array
+// splice - removes/replaces elements
+// sort - sorts an array
