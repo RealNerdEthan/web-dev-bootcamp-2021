8f0a21b2c031013f3064f615b173dbd5bb0079a5
Ethan Goddard <ethangoddard2009@gmail.com>
2021-07-08 10:17:38 -0400

    Section 26 Score Keeper
    Reviewed score keeper project, at direction of instructor I began recreating it before reviewing together. I laid out the structure and styled it, next is to work on the logic.

---
+++ Section 26 - Score Keeper Code Along/Score_Keeper.js
@@ -0,0 +1,17 @@
+const playerOne = document.querySelector('#p1');
+const playerTwo = document.querySelector('#p2');
+const reset = document.querySelector9('#reset');
+
+playerOne.addEventListener('click', function(){
+    //add 1 to first number
+    //check if first nunber = playing to number, then change first number color to green and second number color to red and disable both buttons
+})
+
+playerTwo.addEventListener('click', function(){
+    //add 1 to second number
+    //check if second number = playing to number, then change first number to red and second number to green and disable both buttons
+})
+
+reset.addEventListener('click', function(){
+    //set count to 0 to 0
+})
