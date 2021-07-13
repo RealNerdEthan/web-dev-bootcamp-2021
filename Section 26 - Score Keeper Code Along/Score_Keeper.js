3a3960bbd40c33c0c12c5c0289a04dcfe6ff390c
Ethan Goddard <ethangoddard2009@gmail.com>
2021-07-13 17:04:35 -0400

    PingPong Score Counter
    Made progress on the JS logic. Only issue left is that I have to add 1 more point than the "Playing To" counter is set to in order to trigger the winner logic.

--- Section 26 - Score Keeper Code Along/Score_Keeper.js
+++ Section 26 - Score Keeper Code Along/Score_Keeper.js
@@ -1,17 +1,55 @@
-const playerOne = document.querySelector('#p1');
-const playerTwo = document.querySelector('#p2');
-const reset = document.querySelector9('#reset');
+const playerOneButton = document.querySelector('#p1');
+const playerTwoButton = document.querySelector('#p2');
+const resetButton = document.querySelector('#reset');
+const playerOneDisplay = document.querySelector('#p1Score');
+const playerTwoDisplay = document.querySelector('#p2Score');
+const roundLimitSelector = document.querySelector('#playToAmount')
 
-playerOne.addEventListener('click', function(){
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let roundLimit = 6;
+
+roundLimitSelector.addEventListener('change', function(){
+    roundLimit = parseInt(this.value);
+    //reset();
+})
+
+playerOneButton.addEventListener('click', function(){
     //add 1 to first number
     //check if first nunber = playing to number, then change first number color to green and second number color to red and disable both buttons
+    if (playerOneScore != roundLimit){
+        playerOneScore += 1;
+        playerOneDisplay.textContent = playerOneScore;
+    } else if (playerOneScore === roundLimit){
+        playerOneDisplay.style.color = 'green';
+        playerTwoDisplay.style.color = 'red';
+        playerOneButton.disabled = true;
+        playerTwoButton.disabled = true;
+    }
 })
 
-playerTwo.addEventListener('click', function(){
+playerTwoButton.addEventListener('click', function(){
     //add 1 to second number
     //check if second number = playing to number, then change first number to red and second number to green and disable both buttons
+    if (playerTwoScore != roundLimit){
+        playerTwoScore += 1;
+        playerTwoDisplay.textContent = playerTwoScore;
+    } else if (playerTwoScore === roundLimit){
+        playerOneDisplay.style.color = 'red';
+        playerTwoDisplay.style.color = 'green';
+        playerOneButton.disabled = true;
+        playerTwoButton.disabled = true;
+    }
 })
 
-reset.addEventListener('click', function(){
-    //set count to 0 to 0
-})
+
+resetButton.addEventListener('click', function(){
+    playerOneDisplay.textContent = 0;
+    playerTwoDisplay.textContent = 0;
+    playerOneDisplay.style.color = 'black';
+    playerTwoDisplay.style.color = 'black';
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    playerOneButton.disabled = false;
+    playerTwoButton.disabled = false;
+});
