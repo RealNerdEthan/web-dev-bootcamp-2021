5bef6461c25c2e12c4fbef39edad0e45b60c3f3b
Ethan Goddard <ethangoddard2009@gmail.com>
2021-07-20 09:28:07 -0400

    Fake Request Using Callbacks
    I learned about using callbacks to make requests from an API. I also learned how to make nested callbacks and see how to structure them so that if one fails in the chain, the following don't run.

---
+++ Section 27 - Async JavaScript/fakeRequest_Using_Callbacks.js
@@ -0,0 +1,51 @@
+//This is setup as a fake example of how we'd use a promise to call data from an API. It's mimic a real world example of callback hell and how they work
+
+const fakeRequestCallback = function(url, success, failure){
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(function(){
+        if(delay > 4000) {
+            failure('Connection timed out')
+        } else {
+            success(`Here is your fake data from ${url}`)
+        }
+    }, delay)
+}
+
+const fakeRequestPromise = function(url) {
+    return new Promise(function(resolve, reject){
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(function(){
+            if (delay > 4000){
+                reject('Connection timed out')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+//-------------------------------------------------------------
+//Here is the function that calls the fake data using the functions from above
+
+// fakeRequestCallback('books.com', function(response){
+//     console.log('It worked!', response)
+// }, function(error){
+//     console.log('Fake Error', error)
+// })
+
+//Here are nested callbacks, these are in a chain and if one fails the following one doesn't run
+fakeRequestCallback('books.com/page1', function(response){
+    console.log('1st request worked', response)
+    fakeRequestCallback('books.com/page2', function(response){
+        console.log('2nd request worked', response)
+        fakeRequestCallback('books.com/page3', function(response){
+            console.log('3rd request worked', response)
+        }, function(error){
+            console.log('3rd request error', error)
+        })
+    }, function(error){
+        console.log('2nd request error', error)
+    })
+}, function(error){
+    console.log('1st request error', error)
+})
