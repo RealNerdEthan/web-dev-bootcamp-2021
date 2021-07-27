6ab292220f058fcf1bfe74f4d8f1e928ea8ac5d7
Ethan Goddard <ethangoddard2009@gmail.com>
2021-07-27 12:38:28 -0400

    Update The_Await_Keyword.js
    Learned how to use the await keyword and refactored a color changing project from earlier to reduce complexity and make more efficient/readable.

---
+++ Section 27 - Async JavaScript/The_Await_Keyword.js
@@ -0,0 +1,34 @@
+//This is the promise
+const delayedColorChange = function(color, delay){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+
+async function rainbow(){
+    await delayedColorChange('red', 1000)
+    await delayedColorChange('orange', 1000)
+    await delayedColorChange('yellow', 1000)
+    await delayedColorChange('green', 1000)
+    await delayedColorChange('blue', 1000)
+    await delayedColorChange('indigo', 1000)
+    await delayedColorChange('violet', 1000)
+    return 'All done!'
+}
+
+rainbow().then(function(){
+    console.log('End of the rainbow!')
+})
+
+
+//Here's using another async function but witing until after rainbow() has run
+async function printRainbow(){
+    await rainbow();
+    console.log('Rainbow has finished running.')
+}
+
+printRainbow();
