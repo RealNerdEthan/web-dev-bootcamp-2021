e8fdba7fbc04b94f0af38608d7821bd83cd6544b
Ethan Goddard <ethangoddard2009@gmail.com>
2021-08-02 09:05:13 -0400

    TV Show Search App
    We created a form and requested TV Show API data in order to populate the page.
    
    I also figured out how to clear the images when a new search is submitted by refactoring the code to put the images in a DIV and clearing it upon every new search.

---
+++ Section 28 - AJAX and API's/TV_Show_Search_App.js
@@ -0,0 +1,24 @@
+//This function makes the API data request for whatever search we input and also clears the images when a new search is performed
+const form = document.querySelector('#searchForm');
+const displayArea = document.getElementById('requestImages');
+form.addEventListener('submit', async function(event){
+    event.preventDefault();
+    displayArea.innerHTML = '';
+    const searchTerm =  form.elements.query.value;
+    const config = {params:{q:searchTerm}}
+    const response = await axios.get('https://api.tvmaze.com/search/shows', config);
+    makeImages(response.data);
+    form.elements.query.value = '';
+})
+
+//This function filters out just the medium image URLs and appends them to the page (and only if a medium image is availale) and clears the search box after submission
+const makeImages = function(shows){
+    for(let result of shows){
+        if(result.show.image){
+            const tvShowImage = document.createElement('IMG');
+            tvShowImage.src = result.show.image.medium;
+            displayArea.append(tvShowImage);
+            // document.body.append(tvShowImage);
+        }
+    }
+}
