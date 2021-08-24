7addcc577cf604f336247952248cb1ce92305dc4
Ethan Goddard <ethangoddard2009@gmail.com>
2021-08-24 10:20:19 -0400

    Completed Section 35 and RESTful Demo
    We wrapped up section 35 by finishing out CRUD app with  Update and Delete functionality. We also had to install a Node package called method-override to allow patch and delete requests.
    
    Finally we added a delete button and tested the functionality of every CRUD aspect of the app. Create, Read, Update, Delete.

--- Section 35 - Defining RESTful Routes/RESTDemo/index.js
+++ Section 35 - Defining RESTful Routes/RESTDemo/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 const { v4: uuid } = require('uuid');
 
 //Allows us to submit form data
 app.use(express.urlencoded({ extended: true}))
 //Allows us to submit json data
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 //Set the view engine
 app.set('view engine', 'ejs')
 
 //This is our data array as we don't have a database yet
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -53,19 +55,50 @@ app.post('/comments', function(request, response){
 
 //this grabs the comment by ID and renders the response with the data on /comments/show (normally could be a tweet, a subreddit, anything really that used an ID)
 app.get('/comments/:id', function(request, response){
-    const {id} = request.params;
+    const { id } = request.params;
     const comment = comments.find(c => c.id === id);
     response.render('comments/show', { comment });
 })
 
-app.get('/tacos', function(request, response){
-    response.send('Get /tacos response');
+//A route to setup a form to edit a resource
+app.get('/comments/:id/edit', function(request, response){
+    //Get the comment id
+    const { id } = request.params;
+    // Use id to pass through any info to the form we might need
+    const comment = comments.find(c => c.id === id);
+    //We pre-populated the form with that information
+    response.render('comments/edit', { comment });
 })
 
-app.post('/tacos', function(request, response){
-    const { meat, qty } = request.body;
-    response.send(`Ok, here are your ${qty} ${meat} tacos!`)
+//Setting up a route for patching comments
+app.patch('/comments/:id', function(request, response){
+    //Grab ID from URL
+    const {id} = request.params;
+    //Taking whatever was sent in the request body.comments
+    const newCommentText = request.body.comment;
+    //Find a comment with the matching ID
+    const foundComment = comments.find(c => c.id === id);
+    //Update the comment's comment property with whatever was in request.body.comment
+    foundComment.comment = newCommentText;
+    //Send the user back to the comments index page
+    response.redirect('/comments');
 })
+
+app.delete('/comments/:id', function(request, response){
+    const { id } = request.params;
+    //comments.filter will actually return a new array so we aren't mutating the existing array
+    comments = comments.filter(c => c.id !== id);
+    response.redirect('/comments');
+})
+
+// app.get('/tacos', function(request, response){
+//     response.send('Get /tacos response');
+// })
+
+// app.post('/tacos', function(request, response){
+//     const { meat, qty } = request.body;
+//     response.send(`Ok, here are your ${qty} ${meat} tacos!`)
+// })
 
 app.listen(3000, function(){
     console.log('Listening on port 3000!');
@@ -75,5 +108,5 @@ app.listen(3000, function(){
 //(Index Route) GET /comments - list all comments
 //(Create Route) POST /comments/new - create a new comment
 //(Show Route) GET /comments/:id - get one comments (using ID)
-//(Update Route) PATCH /comments/:id - update one comments (using ID)
+//(Update Route) PATCH /comments/:id/edit - update one comments (using ID)
 //(Destroy Route) DELETE /comments/:id - destroy one comment (using ID)
