fe04714394deddb1f28f2f0d513d4ca486f4608d
Ethan Goddard <ethangoddard2009@gmail.com>
2021-08-24 08:52:01 -0400

    RESTful Routes continued
    We implemented a commenting system that allows us to create and post comments to an index page. We also setup some redirects to get between the index, the comment form, and a details page for the comment.
    
    Then we installed a new node module called UUID that creates a unique ID for each comments and setup a details button that creates an HTML page with a comment and all it's details including the unique ID.

--- Section 35 - Defining RESTful Routes/RESTDemo/index.js
+++ Section 35 - Defining RESTful Routes/RESTDemo/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
 
 //Allows us to submit form data
 app.use(express.urlencoded({ extended: true}))
@@ -13,29 +14,52 @@ app.set('view engine', 'ejs')
 //This is our data array as we don't have a database yet
 const comments = [
     {
-        username: 'Ethan',
-        comment: 'Haha! That is super funny!'
+        id: uuid(),
+        username: 'Todd',
+        comment: 'You should buy Skyrim again!'
     },
     {
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like to go birdwatching and sip coffee on cool mornings.'
     },
     {
+        id: uuid(),
         username: 'Sk8erBoi',
         comment: 'Anyone know where I can find good used board parts?'
     },
     {
+        id: uuid(),
         username: 'onlysaywoof',
         comment: 'woof woof woof'
     }
 ]
 
 app.get('/comments', function(request, response){
-    response.render('comments/index', { comments })
+    response.render('comments/index', { comments });
+})
+
+//This route serves and renders a form
+app.get('/comments/new', function(request, response){
+    response.render('comments/new');
+})
+
+//Where the form submits it's data to and extracts the body, then sending the user back to /comments
+app.post('/comments', function(request, response){
+    const {username, comment} = request.body;
+    comments.push({username, comment, id: uuid() });
+    response.redirect('/comments');
+})
+
+//this grabs the comment by ID and renders the response with the data on /comments/show (normally could be a tweet, a subreddit, anything really that used an ID)
+app.get('/comments/:id', function(request, response){
+    const {id} = request.params;
+    const comment = comments.find(c => c.id === id);
+    response.render('comments/show', { comment });
 })
 
 app.get('/tacos', function(request, response){
-    response.send('Get /tacos response')
+    response.send('Get /tacos response');
 })
 
 app.post('/tacos', function(request, response){
@@ -44,12 +68,12 @@ app.post('/tacos', function(request, response){
 })
 
 app.listen(3000, function(){
-    console.log('Listening on port 3000!')
+    console.log('Listening on port 3000!');
 })
 
 //Our basic CRUD app functionality blueprint:
 //(Index Route) GET /comments - list all comments
-//(Create Route) POST /comments - create a new comment
+//(Create Route) POST /comments/new - create a new comment
 //(Show Route) GET /comments/:id - get one comments (using ID)
 //(Update Route) PATCH /comments/:id - update one comments (using ID)
 //(Destroy Route) DELETE /comments/:id - destroy one comment (using ID)
