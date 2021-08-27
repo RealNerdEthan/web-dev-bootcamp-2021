1c88172d2303d96947e0af9e9e7416fac3b4528b
Ethan Goddard <ethangoddard2009@gmail.com>
2021-08-27 08:21:56 -0400

    Continued working with Mongoose
    We reviewed and practiced connecting Mongoose to Mongo, setting up a model, insert many method, finding with Mongoose, and updating with Mongoose.
    
    About half way through Section 37.

--- Section 37 - Connecting To Mongo With Mongoose/MongooseBasics/index.js
+++ Section 37 - Connecting To Mongo With Mongoose/MongooseBasics/index.js
@@ -1,9 +1,38 @@
 const mongoose = require('mongoose');
+
+//Connect to the MongoDB
 mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
-        console.log("CONNECTION OPEN!!!");
+        console.log("Connection open!");
     })
     .catch(err => {
-        console.log("OH NO ERROR!!!!");
+        console.log("Encountered an error!");
         console.log(err);
+    });
+
+    //Schema is a mapping of different 'collection keys' from Mongo to differernt 'types' in JavaScript
+    const movieSchema = new mongoose.Schema({
+        title: String,
+        year: Number,
+        score: Number,
+        rating: String
+    });
+
+    //Make a model using the above schema
+    const Movie = mongoose.model('Movie', movieSchema);
+    
+    //Make a new instance of movie (must call .save())
+    // const waterWorld = new Movie({title: 'Water World', year: 1995, score: 6.2, rating: 'PG-13'});
+
+    //Insert many instances of movie (not very common) and since it returns a promise we don't need to call .save()
+    Movie.insertMany([
+        { title: 'Amelie', year: 2021, score: 8.3, rating: 'R' },
+        { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+        { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+        { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+        { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13'}
+    ])
+    .then(function(data){
+        console.log('It worked!');
+        console.log(data);
     });
