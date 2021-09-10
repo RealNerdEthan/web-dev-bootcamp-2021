70c493f39178040c7b7dec3b32aa858a2e7af575
Ethan Goddard <ethangoddard2009@gmail.com>
2021-09-10 08:22:18 -0400

    Defining our own middleware
    We learned how to create our own middleware as part of gaining a deeper understanding on how it works

--- Section 40 - Middleware The Key To Express/index.js
+++ Section 40 - Middleware The Key To Express/index.js
@@ -5,6 +5,12 @@ const morgan = require('morgan');
 //Morgan is an example of middleware
 app.use(morgan('dev'));
 
+//Our first middleware
+app.use(function(request, response, next){
+    console.log('This is our first middleware!');
+    next();
+})
+
 app.get('/', function(request, response){
     response.send('Home Page');
 })
